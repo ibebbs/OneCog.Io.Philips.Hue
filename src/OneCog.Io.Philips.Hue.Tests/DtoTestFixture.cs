461c846c3c28f4c6863335ca9acc45ebf0421124
ibebbs <github@bebbs.co.uk>
2015-05-18 13:08:14 +0100

    Added unit tests to confirm deserialization of lights

--- src/OneCog.Io.Philips.Hue.Tests/DtoTestFixture.cs
+++ src/OneCog.Io.Philips.Hue.Tests/DtoTestFixture.cs
@@ -18,5 +18,34 @@ namespace OneCog.Io.Philips.Hue.Tests
 
             Assert.That(state, Is.Not.Null);
         }
+
+        [Test]
+        public void CanDeserializeStateLights()
+        {
+            State state = Serializer.Json.Deserialize<State>(Resources.ConfigurationResponse);
+
+            Assert.That(state, Is.Not.Null);
+            Assert.That(state.Lights, Is.Not.Null);
+            Assert.That(state.Lights.Count, Is.EqualTo(2));
+
+            Dto.Light light = state.Lights[2].Item;
+            Assert.That(light, Is.Not.Null);
+            Assert.That(light.State, Is.Not.Null);
+            Assert.That(light.State.On, Is.True);
+            Assert.That(light.State.Brightness, Is.EqualTo(254));
+            Assert.That(light.State.Hue, Is.EqualTo(33536));
+            Assert.That(light.State.Saturation, Is.EqualTo(144));
+            Assert.That(light.State.Xy, Is.EqualTo(new float[] { 0.3460F, 0.3568F }));
+            Assert.That(light.State.Ct, Is.EqualTo(201));
+            Assert.That(light.State.Alert, Is.EqualTo("none"));
+            Assert.That(light.State.Effect, Is.EqualTo("none"));
+            Assert.That(light.State.ColorMode, Is.EqualTo("hs"));
+            Assert.That(light.State.Reachable, Is.EqualTo(true));
+
+            Assert.That(light.Type, Is.EqualTo("Extended color light"));
+            Assert.That(light.Name, Is.EqualTo("Hue Lamp 2"));
+            Assert.That(light.ModelId, Is.EqualTo("LCT001"));
+            Assert.That(light.FirmwareVersion, Is.EqualTo("65003148"));
+        }
     }
 }
