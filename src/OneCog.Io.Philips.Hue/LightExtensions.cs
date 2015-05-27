255add028064133442b3fb19dd1de95743fa43dd
ibebbs <github@bebbs.co.uk>
2015-05-27 13:53:59 +0100

    Added missing file

---
+++ src/OneCog.Io.Philips.Hue/LightExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Colourful;
+
+namespace OneCog.Io.Philips.Hue
+{
+    public static class LightExtensions
+    {
+        public static Light.ISource AsLightSource(this Dto.Indexed<Dto.Light> light)
+        {
+            return new Light.Bulb
+            {
+                Id = (uint)light.Index,
+                Color = new xyYColor(light.Item.State.Xy[0], light.Item.State.Xy[1], 255.0 / light.Item.State.Brightness)
+            };
+        }
+    }
+}
